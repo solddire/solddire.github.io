c98211443078671706e8191313509db02a95c4c4
solddire <sayganov.kirya@mail.ru>
2023-09-18 21:36:06 +0300

    Добавление блоков наших работ, блока о нас

--- js/slick-slider-services.js
+++ js/slick-slider-services.js
@@ -1,11 +1,11 @@
-$('.multiple-jobs').slick({
+$('.multiple-services').slick({
     infinite: true,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    dots: true,
+    dots: false,
     speed: 700,
-    appendArrows: $('.slick-buttons-jobs'),
+    appendArrows: $('.slick-buttons-services'),
     nextArrow: '<button class="slick_button next_button"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" /></svg></button>',
     prevArrow: '<button class="slick_button prev_button"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" /></svg></button>',
     responsive: [
