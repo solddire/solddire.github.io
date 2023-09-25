6575cef8b78641422e8384e805cab73ae9756cd3
solddire <sayganov.kirya@mail.ru>
2023-09-25 11:00:11 +0300

    fix adaptive

--- js/scroll-header.js
+++ js/scroll-header.js
@@ -4,25 +4,24 @@ let mobileNav = document.querySelector('.mobile-menu-2');
 window.onscroll = function(){
     let scrollWindow = window.pageYOffset;
     let screenWidth = window.screen.width;
-    if (scrollWindow > 1 && navBar.style.backgroundColor != 'none'){
-            if (screenWidth > 1023){
+    if (scrollWindow > 1 && navBar.style.backgroundColor != '#484848' && screenWidth > 768){
+            // if (screenWidth > 1023){
                 navBar.classList.add('blackNav');
-                if (screenWidth < 1024){
-                    mobileNav.classList.add('blackNav');
-                }
-            }
-    }else if(scrollWindow < 1 && navBar.style.backgroundColor != '#ffebcf'){
-        if (screenWidth > 1023){
+            // } else if (screenWidth < 768){
+            //     mobileNav.classList.add('blackNav');
+            // }
+    } else if(scrollWindow < 1 && navBar.style.backgroundColor != 'none' && screenWidth > 768){
+        // if (screenWidth > 1023){
             navBar.classList.remove('blackNav');
-            if (screenWidth < 1024){
-                mobileNav.classList.remove('blackNav');
-            }
-        }
+        // }
+        // else if (screenWidth < 768){
+        //     mobileNav.classList.remove('blackNav');
+        // }
     }
 };
 window.onresize = function(){
     let screenWidth = window.screen.width;
-    if (screenWidth > 1024){
+    if (screenWidth > 1023){
         mobileNav.classList.remove('blackNav');
     }
 };
